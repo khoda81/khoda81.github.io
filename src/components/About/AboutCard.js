c503a88653a399d4b3cf41d5307156f8608a500b
axiom <20.mahdikh.0@gmail.com>
2022-10-25 09:11:48 +0330

    added portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { DiRust } from "react-icons/di";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Mahdi Khodabandeh </span>
+            from <span className="purple"> Guilan, Iran.</span>
+            <br />I am a computer science student in Guilan University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Learning Rust<DiRust />
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Studying 
+            </li>
+          </ul>
+
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
